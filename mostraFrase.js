754a49108f9254aa162c8bf572741e5eb4a83563
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-10-07 15:45:40 +0000

    ajustes

---
+++ mostraFrase.js
@@ -0,0 +1,10 @@
+let cartao = document.getElementById('cartao1')
+const paragrafo = document.createElement('p');
+paragrafo.innerHTML = `Frase do Silvio: "${frases[1]}"`;
+cartao.appendChild(paragrafo);
+
+
+cartao = document.getElementById('cartao1')
+const paragrafo = document.createElement('p');
+paragrafo.innerHTML = `Frase do Silvio: "${frases[1]}"`;
+cartao.appendChild(paragrafo);
